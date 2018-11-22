6d4e2617bca292bc6f0f8dbdaa2cb62788bf4cc6
Jennifer Hurd <jenhurd8@gmail.com>
2018-11-21 22:37:19 -0600

    added eventObject

---
+++ app/src/main/java/com/example/android/chicagotourguideapp/eventObject.java
@@ -0,0 +1,28 @@
+package com.example.android.chicagotourguideapp;
+
+public class eventObject {
+
+    /***single event name, location and image reference ***/
+    private String name;
+    private String location;
+    private String imageId;
+
+    /**single event object constructor***/
+    public eventObject(String name, String location, String imageId){
+        this.name  = name;
+        this.location = location;
+        this.imageId = imageId;
+    }
+
+    /***getters for name, location, and image id***/
+    public String getName(){
+        return name;
+    }
+
+    public String getLocation(){
+        return location;
+    }
+    public String getImageId(){
+        return imageId;
+    }
+}
