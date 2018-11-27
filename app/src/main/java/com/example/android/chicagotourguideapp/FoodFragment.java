0fa1433ef9136aebf1cf95df7af91f340b08a99a
Jennifer Hurd <jenhurd8@gmail.com>
2018-11-26 22:45:36 -0600

    restaurants added

--- app/src/main/java/com/example/android/chicagotourguideapp/FoodFragment.java
+++ app/src/main/java/com/example/android/chicagotourguideapp/FoodFragment.java
@@ -6,7 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,18 +27,32 @@ public class FoodFragment extends Fragment {
         return fragment;
     }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mPage = getArguments().getInt(ARG_PAGE);
-    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        mPage = getArguments().getInt(ARG_PAGE);
+//    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        TextView textView = new TextView(getActivity());
-        textView.setText(R.string.food);
-        return textView;
+
+        View rootView = inflater.inflate(R.layout.fragment_page, container, false);
+
+        ArrayList<EventObject> events = new ArrayList<EventObject>();
+        events.add(new EventObject("Giordano's Pizza", "130 E Randolph St\nChicago, IL 60601\n(312) 616-1200", R.drawable.field_museum));
+        events.add(new EventObject("Portillo's Hot Dogs", "100 W Ontario St\nChicago, IL 60654\n(312) 587-8910", R.drawable.art_institute));
+        events.add(new EventObject("Garrett Popcorn Shops", "625 Michigan Avenue\nChicago, IL 60611\n(888) 476-7267", R.drawable.museum_science_industry));
+        events.add(new EventObject("Al's Beef", "69 W Ontario St\nChicago, IL 60654\n(312) 943-3222", R.drawable.shedd_aquarium));
+        events.add(new EventObject("Harry Caray's Italian Steakhouse", "33 W Kinzie St\nChicago, IL 60654\n(312) 828-0966", R.drawable.adler_planetarium));
+
+        EventAdapter adapter = new EventAdapter(getActivity(), events );
+
+        ListView listView = rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 
 }
