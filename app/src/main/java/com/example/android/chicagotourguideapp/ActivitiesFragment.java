dafe9adc10542712aca0b31cc815ccd56966cdc5
Jennifer Hurd <jenhurd8@gmail.com>
2018-11-30 21:21:57 -0600

    adding more photos

--- app/src/main/java/com/example/android/chicagotourguideapp/ActivitiesFragment.java
+++ app/src/main/java/com/example/android/chicagotourguideapp/ActivitiesFragment.java
@@ -47,11 +47,11 @@ public class ActivitiesFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_page, container, false);
 
         ArrayList<EventObject> events = new ArrayList<EventObject>();
-        events.add(new EventObject("Lincoln Park Zoo", "2001 N Clark St\nChicago, IL 60614\n(312) 742-2000", R.drawable.field_museum));
-        events.add(new EventObject("North Avenue Beach", "1600 N Lake Shore Dr\nChicago, IL 60613\n(773) 363-2223", R.drawable.art_institute));
-        events.add(new EventObject("Maggie Daley Park", "337 E Randolph St\nChicago, IL 60601\n(312) 552-3000", R.drawable.museum_science_industry));
-        events.add(new EventObject("Chicago's First Lady Cruises", "112 E Upper Wacker Dr\nChicago, IL 60601\n(847) 358-1330", R.drawable.shedd_aquarium));
-        events.add(new EventObject("Bobby's Bike Hike", "540 N Lake Shore Dr\nChicago, IL 60611\n(312) 245-9300", R.drawable.adler_planetarium));
+        events.add(new EventObject("Lincoln Park Zoo", "2001 N Clark St\nChicago, IL 60614\n(312) 742-2000", R.drawable.lincoln_park_zoo));
+        events.add(new EventObject("North Avenue Beach", "1600 N Lake Shore Dr\nChicago, IL 60613\n(773) 363-2223", R.drawable.north_avenue_beach));
+        events.add(new EventObject("Maggie Daley Park", "337 E Randolph St\nChicago, IL 60601\n(312) 552-3000", R.drawable.maggie_daley_park));
+        events.add(new EventObject("Chicago's First Lady Cruises", "112 E Upper Wacker Dr\nChicago, IL 60601\n(847) 358-1330", R.drawable.cruise));
+        events.add(new EventObject("Bobby's Bike Hike", "540 N Lake Shore Dr\nChicago, IL 60611\n(312) 245-9300", R.drawable.bobby_s_historic_neighborhoods));
 
         EventAdapter adapter = new EventAdapter(getActivity(), events );
 
